0b9fef80f9716427b0095d08f50739d1701c6676
mmarcinkowski <michal.marcinkowski@ragnarson.com>
2017-02-16 17:08:43 +0100

    Show newsletter popup conditionally

---
+++ source/javascripts/newsletter.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var newsletterEl = document.getElementsByClassName('newsletter-popup')[0];
+  var newsletterCloseBtnEl = document.getElementById('newsletter-close-btn');
+
+  newsletterCloseBtnEl.addEventListener('click', function(e) {
+    newsletterEl.style.display = 'none';
+    document.cookie = 'seenNewsletterPopup=true';
+    e.preventDefault();
+  });
+
+  if(!readCookie('seenNewsletterPopup')) {
+    newsletterEl.style.display = 'block';
+  }
+});
+
+function readCookie(name) {
+  var nameEQ = name + '=';
+  var splittedCookie = document.cookie.split(';');
+
+  for(var i = 0; i < splittedCookie.length; i++) {
+    var cookie = splittedCookie[i];
+    while (cookie.charAt(0) == ' ') cookie = cookie.substring(1, cookie.length);
+    if(cookie.indexOf(nameEQ) == 0) return cookie.substring(nameEQ.length, cookie.length);
+  }
+
+  return null;
+}
